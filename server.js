2258ea4dd23a9a8d694be6c89e55495a817b362d
KumarSahoo <sahoodhirenkumar10@gmail.com>
2026-04-15 23:41:31 +0530

    Update server.js

--- server.js
+++ server.js
@@ -19,3 +19,44 @@ app.listen(3000, () => {
 app.get("/test", (req, res) => {
   res.send("API working");
 });
+
+
+const express = require("express");
+const app = express();
+
+app.use(express.json());
+
+let users = [];
+
+app.get("/", (req, res) => {
+  res.send("Server Running");
+});
+
+app.get("/test", (req, res) => {
+  res.send("API working");
+});
+
+app.post("/signup", (req, res) => {
+  const { username, password } = req.body;
+
+  users.push({ username, password });
+  res.send("User registered");
+});
+
+app.post("/login", (req, res) => {
+  const { username, password } = req.body;
+
+  const user = users.find(
+    u => u.username === username && u.password === password
+  );
+
+  if (user) {
+    res.send("Login successful");
+  } else {
+    res.send("Invalid credentials");
+  }
+});
+
+app.listen(3000, () => {
+  console.log("Server started");
+});
