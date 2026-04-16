32c18b1c859b092c939868bee4a58d79c1b00b18
KumarSahoo <sahoodhirenkumar10@gmail.com>
2026-04-16 21:05:57 +0530

    Update script.js

--- script.js
+++ script.js
@@ -21,4 +21,26 @@ function loginUser() {
         }
     });
 }
+function signupUser() {
+    const username = document.getElementById("username").value;
+    const password = document.getElementById("password").value;
+
+    fetch("https://classroom-test-system.onrender.com/signup", {
+        method: "POST",
+        headers: {
+            "Content-Type": "application/json"
+        },
+        body: JSON.stringify({ username, password })
+    })
+    .then(res => res.text())
+    .then(data => {
+        alert(data);
+
+        // 🔥 Redirect to login page after signup
+        window.location.href = "login.html";
+    })
+    .catch(err => {
+        console.log(err);
+    });
+}
 
